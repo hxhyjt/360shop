44d90bdce4a0e016b2e773e9b476e3b770fad04b
hxhyjt <627795822@qq.com>
2016-07-25 15:18:16 +0800

    news

---
+++ js/index.js
@@ -0,0 +1,43 @@
+$(function(){
+	var mySwiper = new Swiper ('.swiper-container', {
+		direction: 'vertical',
+		loop: true,
+		slidesPerView: 1,
+		paginationClickable: true,
+		spaceBetween: 0,
+		autoplay: 2500,
+		autoplayDisableOnInteraction: false,
+		
+		// 如果需要分页器
+		pagination: '.swiper-pagination',
+		
+		// // 如果需要前进后退按钮
+		// nextButton: '.swiper-button-next',
+		// prevButton: '.swiper-button-prev',
+		
+		// // 如果需要滚动条
+		// scrollbar: '.swiper-scrollbar',
+	})
+	// console.log(mySwiper)
+})
+
+
+
+
+
+
+
+/*
+pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        
+        
+        
+        loop: true,
+        
+        
+
+
+
+*/ 
